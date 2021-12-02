795ee509b62be180fc6ff662b32da4fa034fce2c
Knut Ahlers <knut@ahlers.me>
2021-12-02 01:49:08 +0100

    Fix: Update catalog api url for vault download
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- build.sh
+++ build.sh
@@ -39,7 +39,7 @@ echo "jenkins ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers.d/jenkins
 usermod -a -G docker jenkins
 
 # Install Vault for global usage
-VAULT_VERSION=$(curl -sSfL "https://lv.luzifer.io/catalog-api/vault/latest.txt?p=version")
+VAULT_VERSION=$(curl -sSfL "https://lv.luzifer.io/v1/catalog/vault/latest/version")
 curl -sSfLo vault.zip "https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip"
 unzip vault.zip
 rm vault.zip
